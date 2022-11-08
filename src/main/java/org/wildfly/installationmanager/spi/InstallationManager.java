3f176a9e8d3cbf6eec8bce9b4806085742d3687c
Bartosz Spyrko-Smietanko <bspyrkos@redhat.com>
2022-11-08 12:50:22 +0000

    Initial channel management ops

--- src/main/java/org/wildfly/installationmanager/spi/InstallationManager.java
+++ src/main/java/org/wildfly/installationmanager/spi/InstallationManager.java
@@ -1,8 +1,10 @@
 package org.wildfly.installationmanager.spi;
 
+import org.wildfly.installationmanager.Channel;
 import org.wildfly.installationmanager.HistoryResult;
 import org.wildfly.installationmanager.ArtifactChange;
 
+import java.util.Collection;
 import java.util.List;
 
 public interface InstallationManager {
@@ -39,4 +41,38 @@ public interface InstallationManager {
      * @throws Exception
      */
     List<ArtifactChange> findUpdates() throws Exception;
+
+    /**
+     * Lists channels the server installation is subscribed to.
+     * If the servers is not subscribed to any channels, empty list is returned.
+     *
+     * @return Collection of {@code Channel}
+     * @throws Exception - if unable to read the installation metadata
+     */
+    Collection<Channel> listChannels() throws Exception;
+
+    /**
+     * Unsubscribes the server installation from a channel.
+     *
+     * @param channel - {@code Channel} to be removed.
+     * @throws Exception - if unable to read the installation metadata, or the Channel index doesn't exist
+     */
+    void removeChannel(Channel channel) throws Exception;
+
+    /**
+     * Subscribe the server installation to a new channel.
+     *
+     * @param channel - new {@code Channel}
+     * @throws Exception - if unable to read the installation metadata, or the Channel index doesn't exist
+     */
+    void addChannel(Channel channel) throws Exception;
+
+    /**
+     * Persists changes to a channel that the server installation is subscribed to.
+     *
+     * @param oldChannel
+     * @param newChannel - modified {@code Channel} to be stored.
+     * @throws Exception
+     */
+    public void changeChannel(Channel oldChannel, Channel newChannel) throws Exception;
 }
