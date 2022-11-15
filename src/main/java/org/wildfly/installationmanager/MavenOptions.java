458a2a869b446ef5cb57a1fb82276180c5d5e009
Yeray Borges <yborgess@redhat.com>
2022-11-15 18:24:20 +0000

    Add logic to automatically the local repository if no repository is specified

--- src/main/java/org/wildfly/installationmanager/MavenOptions.java
+++ src/main/java/org/wildfly/installationmanager/MavenOptions.java
@@ -1,13 +1,17 @@
 package org.wildfly.installationmanager;
 
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class MavenOptions {
+    public static final Path LOCAL_MAVEN_REPO = Paths.get(System.getProperty("user.home"), ".m2", "repository");
+
     private Path localRepository;
     private boolean offline;
 
+
     public MavenOptions(Path localRepository, boolean offline) {
-        this.localRepository = localRepository;
+        this.localRepository = localRepository == null ? LOCAL_MAVEN_REPO : localRepository;
         this.offline = offline;
     }
 
